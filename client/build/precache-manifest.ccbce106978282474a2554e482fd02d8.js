68c3356d7cb030b77af4b28683ab5d70d640e970
JohnDaise <jdaise@gmail.com>
2020-06-27 09:15:27 -0400

    build setup and small fix

---
+++ client/build/precache-manifest.ccbce106978282474a2554e482fd02d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61cb8323efc9d5006de7",
+    "url": "/static/css/main.e38df0d8.chunk.css"
+  },
+  {
+    "revision": "61cb8323efc9d5006de7",
+    "url": "/static/js/main.9491fee4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f14e103a85ded4a2db8",
+    "url": "/static/js/2.9f08725b.chunk.js"
+  },
+  {
+    "revision": "db78084d16afd77ba101e0f8492168d2",
+    "url": "/index.html"
+  }
+];
